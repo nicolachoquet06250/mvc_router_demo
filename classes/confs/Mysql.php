5c38e4354d17b8bc83d7b30aa3776bf123d33c45
nicolas.choquet <nicolas.choquet@lagardere-active.com>
2019-08-09 13:37:04 +0200

    update structure multi site for commands

--- classes/confs/Mysql.php
+++ classes/confs/Mysql.php
@@ -4,8 +4,4 @@
 namespace mvc_router\confs\custom;
 
 
-class Mysql extends \mvc_router\confs\Mysql {
-	protected $host = 'mysqli';
-	protected $pass = 'root';
-	protected $user = 'root';
-}
+class Mysql extends \mvc_router\confs\Mysql {}
