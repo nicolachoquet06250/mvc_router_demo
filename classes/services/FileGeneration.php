6aba77bc085453f910d865d66bc82e49bcf6d141
Nicolas Choquet <nchoquet@norsys.fr>
2019-10-29 14:42:52 +0100

    Documentation finished

--- classes/services/FileGeneration.php
+++ classes/services/FileGeneration.php
@@ -8,9 +8,12 @@
 			parent::generate_base_htaccess( $custom_dir );
 			$slash = $this->helpers->get_slash();
 			$htaccess_php = '<?php
-
-mvc_router\dependencies\Dependency::get_wrapper_factory()->get_dependency_wrapper()->get_router()
-	->root_route(\'home_controller\')->inspect_controllers();
+	try {
+		mvc_router\dependencies\Dependency::get_wrapper_factory()->get_dependency_wrapper()->get_router()
+			->root_route( \'home_controller\' )->inspect_controllers();
+	} catch( Exception $e ) {
+		exit($e->getMessage());
+	}
 ';
 			file_put_contents(($this->helpers->is_unix() ? __DIR__.$slash.'..'.$slash.'..'.$slash : '').$custom_dir.$slash.'htaccess.php', $htaccess_php);
 		}
